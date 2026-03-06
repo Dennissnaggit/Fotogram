ee33f6a2e2ee91452af7d796ff80479997271c7e
Dennis Falkner <dennis.falkner@snaggit.app>
2026-03-06 11:40:03 +0100

    render funktion für das Dialog gebaut

--- script.js
+++ script.js
@@ -19,11 +19,32 @@ function render() {
 
 function getImages(index) {
   return `
-        <img onclick=("openDialog()") class="main-images" src= "${myImages[index]}" alt="Weltraum Bilder">
+        <img onclick=(openDialog()) class="main-images" src= "${myImages[index]}" alt="Weltraum Bilder">
         `;
 }
 
 function openDialog() {
-    dialogRef.showModal();
+  dialogRef.replaceChildren();
+  dialogRef.showModal();
+  dialogRef.innerHTML += renderDialog;
 }
-openDialog();
+
+function renderDialog() {
+  return ` 
+          <header class="dialog-header">
+            <h2>Image Name</h2>
+            <a href="#close"><img src="/img/Close icon.svg" alt="schliessen button" /></a>
+          </header>
+          <section class="dialog-image">
+            <img
+              class="dialog-main-image"
+              src="/img/pictures/All_1.png"
+              alt=""
+            />
+          </section>
+          <footer>
+            <button><img src="/img/buttonleft.png" alt="" /></button>
+            <p>1/12</p>
+            <button><img src="/img/buttonright.png" alt="" /></button>
+          </footer>`;
+}
